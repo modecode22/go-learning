c1f6070ebc7586e050977c88fbdd3678126c558a
moncef <epiccoder00@gmail.com>
2023-10-08 20:16:20 +0100

    hello world in go

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name string = "Moncef"
+    var age int = 30
+    var isProgrammer bool = true
+
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Is a Programmer?", isProgrammer)
+}
+
+
+
